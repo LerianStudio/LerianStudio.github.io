36c8d83cbfe5edec9052f201bc75b95897f58528
MartinezAvellan <martinez.avellan@hotmail.com>
2024-12-02 14:50:46 +0100

    feature: test commit swagger

---
+++ swagger-ui-bundle.js
@@ -0,0 +1,10 @@
+urls: [
+    {
+      url: "https://github.com/LerianStudio/midaz/blob/main/components/ledger/api/swagger.json",
+      name: "Ledger"
+    },
+    {
+        url: "https://github.com/LerianStudio/midaz/blob/main/components/transaction/api/swagger.json",
+        name: "Transaction"
+    },    
+  ]
